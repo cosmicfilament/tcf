7e8988a66c86fba91e88949316ab35fe98063fc6
john@thecosmicfilament.com <john@thecosmicfilament.com>
2017-10-23 17:11:43 -0400

    more webpack stuff with images

--- public/require.js
+++ public/require.js
@@ -1,11 +1,36 @@
 'use strict';
 
+import "./img/tcf-monet.jpg";
+import "./img/tcf-logo.jpg";
+import "./img/tcf-john.jpg";
+import "./img/tcf-icon-wh.png";
+import "./img/tcf-icon.png";
+
+import "./img/portfolio/GoldFinch.jpg";
+import "./img/portfolio/ziggy.jpg";
+import "./img/portfolio/NGC6888.jpg";
+import "./img/portfolio/goats-custer.jpg";
+import "./img/portfolio/man-solomons.jpg";
+import "./img/portfolio/Badlands.jpg";
+import "./img/portfolio/GoldFinch-thumb.jpg";
+import "./img/portfolio/ziggy-thumb.jpg";
+import "./img/portfolio/NGC6888-thumb.jpg";
+import "./img/portfolio/goats-custer-thumb.jpg";
+import "./img/portfolio/man-solomons-thumb.jpg";
+import "./img/portfolio/Badlands-thumb.jpg";
+import "./img/portfolio/rainbow.jpg";
+import "./img/portfolio/whaleshark.jpg";
+import "./img/portfolio/fins.jpg";
+import "./img/portfolio/rainbow-thumb.jpg";
+import "./img/portfolio/whaleshark-thumb.jpg";
+import "./img/portfolio/fins-thumb.jpg";
+
 import "./vendor/animate/animate.css";
 import "./vendor/elasic-slider/elastic.css";
 import "./vendor/iconmoon/linea-icon.css";
 import "./vendor/owl-carousel/owl.carousel.css";
 import "./vendor/owl-carousel/owl.theme.css";
-import 'font-awesome/css/font-awesome.css';
+import './vendor/font-awesome/css/font-awesome.css';
 import "./vendor/assets/css/shortcodes.css";
 import "./vendor/assets/css/style.css";
 import "./vendor/assets/css/default-theme.css";
@@ -21,7 +46,6 @@ import "./vendor/elasic-slider/jquery.eislideshow.js";
 import "./vendor/flex-slider/jquery.flexslider-min.js";
 import "./vendor/images-loaded/imagesloaded.js";
 import "./vendor/isotope/jquery.isotope.js";
-//import "./vendor/mailchimp/jquery.ajaxchimp.min.js";
 import "./vendor/menuzord/menuzord.js";
 import "./vendor/nav/jquery.nav.js";
 import "./vendor/owl-carousel/owl.carousel.min.js";
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-'use strict';
+"use strict";
 
-const webpack = require('webpack');
-const path = require('path');
-//const merge = require('webpack-merge');
-//const glob = require('glob');
+const webpack = require("webpack");
+const path = require("path");
+//const merge = require("webpack-merge");
+//const glob = require("glob");
 
 const PATHS = {
-    in: path.join(__dirname, 'public'),
-    out: path.join(__dirname, 'public/dist'),
+    in: path.join(__dirname, "public"),
+    out: path.join(__dirname, "public/dist"),
 };
 
 module.exports = {
     cache: true,
     entry: [
         "webpack-bootstrap-installer",
-        path.resolve(PATHS.in, 'require.js'),
+        path.resolve(PATHS.in, "require.js"),
     ],
     output: {
         path: PATHS.out,
-        filename: 'bundle.js',
+        filename: "bundle.js",
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
             jquery: "jquery",
-            "window.jQuery": "jquery'",
+            "window.jQuery": "jquery",
             "window.$": "jquery"
         })
     ],
@@ -36,17 +36,33 @@ module.exports = {
                 test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "url-loader",
                 options: {
-                    limit: '10000',
-                    name: '/fonts/[name].[ext]',
+                    limit: "10000",
+                    name: "/fonts/[name].[ext]",
                     publicPath: "public/dist",
                 },
             },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    'url-loader?limit=10000&name=img/[name].[ext]',
-                    'img-loader?name=img/[name].[ext]',
-                ],
+                // use: [
+                //     "url-loader?limit=10000&name=img/[name].[ext]",
+                //     "img-loader?name=img/[name].[ext]",
+                // ],
+                loader: "url-loader",
+                options: {
+                    limit: "10000",
+                    name: "/img/[name].[ext]",
+                    publicPath: "public/dist",
+
+                },
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: "img-loader",
+                options: {
+                    name: "/img/[name].[ext]",
+                    publicPath: "public/dist",
+
+                },
             },
             {
                 test: /\.css$/,
@@ -55,13 +71,13 @@ module.exports = {
             {
                 test: /.js$/,
                 include: [
-                    path.resolve(__dirname, './public/js'),
+                    path.resolve(__dirname, "./public/js"),
                 ],
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 query: {
-                    presets: ['babel-preset-env']
+                    presets: ["babel-preset-env"]
                 }
-            }
+            },
         ]
     }
 };
